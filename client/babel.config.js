7be07e12aa6d4526918e6dbe2003926f838f12ff
Ollie <126629452+ooloughlin@users.noreply.github.com>
2025-04-10 00:01:49 +1200

    Fix testing files, added babel

---
+++ client/babel.config.js
@@ -0,0 +1,4 @@
+﻿/// <reference types="cypress" />
+module.exports = {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+};
--- server/tests/test_sanity.py
+++ server/tests/test_sanity.py
@@ -1,23 +1,2 @@
-<html>
-<head>
-<title>test_sanity.py</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<style type="text/css">
-.s0 { color: #cf8e6d;}
-.s1 { color: #bcbec4;}
-.s2 { color: #56a8f5;}
-.s3 { color: #bcbec4;}
-.s4 { color: #2aacb8; font-style: italic;}
-</style>
-</head>
-<body bgcolor="#1e1f22">
-<table CELLSPACING=0 CELLPADDING=5 COLS=1 WIDTH="100%" BGCOLOR="#606060" >
-<tr><td><center>
-<font face="Arial, Helvetica" color="#000000">
-test_sanity.py</font>
-</center></td></tr></table>
-<pre><span class="s0">def </span><span class="s2">test_pytest_works</span><span class="s3">():</span>
-    <span class="s0">assert </span><span class="s4">2 </span><span class="s3">+ </span><span class="s4">2 </span><span class="s3">== </span><span class="s4">4</span>
-</pre>
-</body>
-</html>
+def test_pytest_works():
+    assert 2 + 2 == 4
