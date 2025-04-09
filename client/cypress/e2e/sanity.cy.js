85667f4a56e90d71d36e18220c00b4975f1dadf5
Ollie <126629452+ooloughlin@users.noreply.github.com>
2025-04-10 00:27:38 +1200

    fixed sanity.test.jsx format, corrected label sanity.cy.js

--- client/cypress/e2e/sanity.cy.js
+++ client/cypress/e2e/sanity.cy.js
@@ -2,6 +2,6 @@
 describe('Cypress is working', () => {
     it('visits the local app and checks for something', () => {
         cy.visit('http://localhost:5173'); // Make sure you're running `npm run dev`
-        cy.contains('Import').should('exist'); // Or change to any keyword in your app
+        cy.contains('Import Exam (XML)').should('exist'); // Or change to any keyword in your app
     });
 });
--- client/src/components/Sanity.test.jsx
+++ client/src/components/Sanity.test.jsx
@@ -1,23 +1,7 @@
-<html>
-<head>
-<title>Sanity.test.jsx</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<style type="text/css">
-.s0 { color: #bcbec4;}
-.s1 { color: #bcbec4;}
-.s2 { color: #6aab73;}
-.s3 { color: #d5b778;}
-</style>
-</head>
-<body bgcolor="#1e1f22">
-<table CELLSPACING=0 CELLPADDING=5 COLS=1 WIDTH="100%" BGCOLOR="#606060" >
-<tr><td><center>
-<font face="Arial, Helvetica" color="#000000">
-Sanity.test.jsx</font>
-</center></td></tr></table>
-<pre><span class="s0">test</span><span class="s1">(</span><span class="s2">'Jest is working'</span><span class="s1">, () =&gt; {</span>
-    <span class="s0">render</span><span class="s1">(</span><span class="s3">&lt;div&gt;</span><span class="s0">Hello, test!</span><span class="s3">&lt;/div&gt;</span><span class="s1">);</span>
-    <span class="s0">expect</span><span class="s1">(</span><span class="s0">screen</span><span class="s1">.</span><span class="s0">getByText</span><span class="s1">(</span><span class="s2">'Hello, test!'</span><span class="s1">)).</span><span class="s0">toBeInTheDocument</span><span class="s1">();</span>
-<span class="s1">});</span></pre>
-</body>
-</html>
+import { render, screen } from '@testing-library/react';
+import MyComponent from './MyComponent';
+
+test('renders something', () => {
+    render(<MyComponent />);
+    expect(screen.getByText(/something/i)).toBeInTheDocument();
+});
