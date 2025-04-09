8d1424b3130fa709ae90db781ad535b12dfd4396
Ollie <126629452+ooloughlin@users.noreply.github.com>
2025-04-10 00:43:31 +1200

    fixed import in sanity.test.jsx

--- client/src/components/Sanity.test.jsx
+++ client/src/components/Sanity.test.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 test('renders simple text', () => {
     render(<div>Hello Cache Converters!</div>);
-    expect(screen.getByText('Hello world')).toBeInTheDocument();
+    expect(screen.getByText('Hello Cache Converters!')).toBeInTheDocument();
 });
