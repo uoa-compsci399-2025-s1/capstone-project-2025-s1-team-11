18f45369295a223ab4964bf66f195beff01436c5
Ollie <126629452+ooloughlin@users.noreply.github.com>
2025-04-10 00:51:21 +1200

    added jest.config.js

---
+++ client/jest.config.js
@@ -0,0 +1,8 @@
+﻿// jest.config.js
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // optional but recommended
+    transform: {
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+};
