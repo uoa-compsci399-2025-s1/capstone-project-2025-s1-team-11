b2a7d6df2dcf444cd5c731f2d975bb1cf130e0aa
Danny Weng <danny26574@gmail.com>
2025-04-11 13:58:30 +1200

    design changes to home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,12 +1,36 @@
-// src/pages/ExamFileManager.jsx
 import React from "react";
+import homePageImage from "../assets/homePageImage.jpg";
 
-const Home = ({  }) => {
-
+const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Welcome to Assessly, by Cache Converters</p>
+        <div style={{ position: "relative", width: "100%", maxHeight: "600px", overflow: "hidden" }}>
+            {/* Background Image */}
+            <img 
+                src={homePageImage} 
+                alt="Home" 
+                style={{ 
+                    width: "100%", 
+                    height: "600px", 
+                    objectFit: "cover", 
+                    display: "block" 
+                }} 
+            />
+
+            {/* Overlay Text */}
+            <div 
+                style={{
+                    position: "absolute",
+                    top: "25%", 
+                    left: "5%", 
+                    color: "black",
+                    textAlign: "left"
+                }}
+            >
+                <h1 style={{ margin: 0, fontSize: "3rem" }}>Home</h1>
+                <p style={{ marginTop: "1rem", fontSize: "1.25rem" }}>
+                    Welcome to Assessly, by Cache Converters.
+                </p>
+            </div>
         </div>
     );
 };
