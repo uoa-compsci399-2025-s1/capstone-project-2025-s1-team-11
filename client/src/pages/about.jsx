a7517d5ec3c3e29d301078a89281c77133443812
Danny Weng <danny26574@gmail.com>
2025-04-16 15:42:05 +1200

    Written description for About page.

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -5,9 +5,20 @@ const About = () => {
         <div>
             <h1>About</h1>
             <p>
-                Assessly is designed to be a lightweight, streamlined, user-friendly web application platform developed to create an efficient process for creating, marking, and analysing multiple-choice question (MCQ) examination papers.
+                Our team, Cache Converters, has developed Assessly as a lightweight, streamlined, user-friendly web application platform designed to create an efficient process for creating, marking, and analysing multiple-choice question (MCQ) examination papers. 
                 The idea behind Assessly was born from challenges faced by academic staff at the University of Auckland, and the development of this project was designed with accessibility and ease of use in mind, especially for those without technical backgrounds. 
-                Through this application, staff can spend less time on tedious formatting and manual workflow, instead leaving more time for teaching and improving assessment quality. 
+                Through this application, staff can spend less time on tedious formatting and manual workflow instead of leaving more time for teaching and improving assessment quality. 
+            </p>
+            <p>
+                The central vision of Assessly is to simplify and enhance the entire lifecycle of MCQ-style examinations. 
+                The process begins with generating and building multiple randomised versions of a singular exam, part of standard practice to improve fairness and as a preventative measure for cheating. 
+                From that point, Assessly can accept scanned teleform data for an automatic marking process, providing detailed statistical reports and insights for analysis, thereby ensuring the creation and evaluation of exams are dependable and standardised. 
+                With real user stories in mind, one of the key goals for Assessly is to ensure that it is easy for all staff, regardless of technical expertise, and that lecturers across all faculties can generate compliant and high-quality MCQ exams. 
+                Previously, this process was highly time-consuming, error-prone, and reliant on a few staff members within our Computer Science department. 
+                Still, by removing this bottleneck, our tool creates accessibility with a wide range of flexible options to support any needs, including format flexibility in exam sources, such as DOCX, LaTeX, and XML-based work.
+            </p>
+            <p>
+                We hope this tool can improve the process of generating, distributing, and assessing exams consolidated into a single lightweight and intuitive platform, improving the academic experience for educators. 
             </p>
         </div>
     );
