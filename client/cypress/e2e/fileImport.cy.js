0b0edf5e0705eee817ba3d8f8e7b5773b8326d38
Ollie <126629452+ooloughlin@users.noreply.github.com>
2025-04-20 16:39:11 +1200

    docxDTO WIP

--- client/cypress/e2e/fileImport.cy.js
+++
@@ -1,8 +0,0 @@
-﻿describe('File Import Flow', () => {
-    it('uploads a .qti file through the UI', () => {
-        cy.visit('http://localhost:5173/builder');
-
-        cy.get('input[type="file"]').attachFile('example.qti');
-        cy.contains(/exam uploaded/i).should('exist');
-    });
-});
