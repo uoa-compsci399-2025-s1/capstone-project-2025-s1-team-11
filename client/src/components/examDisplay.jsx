fe819e64224c7e106a4868956c1e944cb5e1f877
Brodie McInnes <111727900+brodiemcinnes@users.noreply.github.com>
2025-04-24 18:24:05 +1200

    mcq builder pagination

--- client/src/components/examDisplay.jsx
+++ client/src/components/examDisplay.jsx
@@ -318,7 +318,7 @@ const ExamDisplay = () => {
               questionsIndex,
             };
           })}
-          pagination={false}
+          pagination={{ pageSize: 10, showSizeChanger: true, pageSizeOptions: ['10', '20', '50', '100'] }}
           scroll={{ x: "max-content" }}
         />
       </DndContext>
