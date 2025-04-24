343146fb78ce10fca733d2da81ada16c63389d38
Brodie McInnes <111727900+brodiemcinnes@users.noreply.github.com>
2025-04-24 17:28:33 +1200

    a

--- client/src/pages/builder.jsx
+++ client/src/pages/builder.jsx
@@ -34,7 +34,7 @@ const Builder = () => {
                             Export functions coming soon
                         </Typography.Paragraph>
                         <div style={{ marginTop: 16 }}>
-                            <Button type="default" style={{ marginRight: 8 }}>
+                            <Button type="default" style={{ marginRight: 8 }}> {/* does not do anything, to be */}
                                 Download Exam
                             </Button>
                             <Button type="primary" onClick={() => navigate('/randomiser')}>
