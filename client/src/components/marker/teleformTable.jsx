eab77007a20875fdcfc7d977de56303b0bbb916f
William <19891289+zZoogZz@users.noreply.github.com>
2025-05-13 16:32:15 +1200

    Added support for new answerkey grid

--- client/src/components/marker/teleformTable.jsx
+++ client/src/components/marker/teleformTable.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Table } from "antd";
 import AnswerGrid from "./AnswerGrid";
 
-const columns = [
+const columns = (answerKey) => [
   { title: 'FIRSTNAME', dataIndex: 'firstName', key: 'firstName' },
   { title: 'LASTNAME', dataIndex: 'lastName', key: 'lastName' },
   { title: 'ID', dataIndex: 'studentId', key: 'studentId' },
@@ -11,15 +11,15 @@ const columns = [
     title: 'ANSWERS',
     dataIndex: 'answerString',
     key: 'answerString',
-    render: (text) => <AnswerGrid answerString={text} />
+    render: (text) => <AnswerGrid answerString={text} answerKey={answerKey} />
   }
 ];
 
-const TeleformTable = ({ data }) => (
+const TeleformTable = ({ data, answerKey }) => (
   <Table
-    columns={columns}
+    columns={columns(answerKey)}
     dataSource={data.map((row, idx) => ({ ...row, key: idx }))}
-    pagination={{ pageSize: 5, showSizeChanger: true, pageSizeOptions: ['5', '10', '20'] }}
+    pagination={{ pageSize: 5, showSizeChanger: true }}
     bordered
     scroll={{ x: true }}
   />
