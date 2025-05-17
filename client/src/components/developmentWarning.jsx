f4af68a399bc4f83024c26ee0d86c6f92d77c3ed
William <19891289+zZoogZz@users.noreply.github.com>
2025-05-17 15:40:05 +1200

    Update developmentWarning text

--- client/src/components/developmentWarning.jsx
+++ client/src/components/developmentWarning.jsx
@@ -5,7 +5,7 @@ const DevelopmentWarning = () => {
     return (
         <Alert
             message="Warning"
-            description="Assessly is in early development. Features may be incomplete and bugs are expected."
+            description="Assessly is in development. Features should be complete, however bugs are expected. Please report any bugs you encounter to Team 11. "
             type="warning"
             showIcon
             closable
