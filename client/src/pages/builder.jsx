411f801f9a71237b8ac6b61f103f4c8d01ebc58b
Danny Weng <danny26574@gmail.com>
2025-05-20 02:39:07 +1200

    Deleted progress wrapper, no longer renders stages

--- client/src/pages/builder.jsx
+++ client/src/pages/builder.jsx
@@ -5,8 +5,7 @@ import { selectExamData } from "../store/exam/selectors.js";
 import ExamDisplay from "../components/examDisplay.jsx";
 import ExamFileManager from "../components/ExamFileManager.jsx";
 import ExamSidebar from "../components/ExamSidebar.jsx";
-import MCQBuilderProgressWrapper from "../components/MCQBuilderProgressWrapper.jsx";
-import { Typography, Button, Space, Row, Col, Tooltip } from "antd";
+import { Typography, Button, Space, Row, Col, Tooltip, Divider } from "antd";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { exportExamToPdf } from "../services/exportPdf.js";
 
@@ -29,123 +28,90 @@ const Builder = () => {
         setSidebarCollapsed(!sidebarCollapsed);
     };
 
-    const renderStageContent = (step) => {
-        switch (step) {
-            case 0:
-                return (
-                    <div>
-                        <Typography.Title level={3}>Cover Page</Typography.Title>
-                    </div>
-                );
-            case 1:
-                return (
-                    <>
-                        {/* Sidebar Toggle Button with label now above the table */}
-                        <div style={{ 
-                            display: 'flex', 
-                            justifyContent: 'flex-end', 
-                            marginBottom: 16
-                        }}>
-                            <Tooltip title={sidebarCollapsed ? "Show Sidebar" : "Hide Sidebar"}>
-                                <Button 
-                                    type="default"
-                                    icon={sidebarCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                                    onClick={toggleSidebar}
-                                >
-                                    {sidebarCollapsed ? "Show Sidebar" : "Hide Sidebar"}
-                                </Button>
-                            </Tooltip>
+    return (
+        <div style={{ padding: '24px' }}>
+            <h1>Builder</h1>
+            
+            {/* Cover Page Section */}
+            <div style={{ marginBottom: '48px' }}>
+                <Typography.Title level={3}>Cover Page</Typography.Title>
+            </div>
+
+            <Divider />
+
+            {/* MCQ Exam Questions Section */}
+            <div style={{ marginBottom: '48px' }}>
+                <div style={{ 
+                    display: 'flex', 
+                    justifyContent: 'flex-end', 
+                    marginBottom: 16
+                }}>
+                    <Tooltip title={sidebarCollapsed ? "Show Sidebar" : "Hide Sidebar"}>
+                        <Button 
+                            type="default"
+                            icon={sidebarCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                            onClick={toggleSidebar}
+                        >
+                            {sidebarCollapsed ? "Show Sidebar" : "Hide Sidebar"}
+                        </Button>
+                    </Tooltip>
+                </div>
+                
+                <Row gutter={24}>
+                    <Col xs={24} xl={sidebarCollapsed ? 24 : 18} style={{ transition: 'width 0.3s' }}>
+                        <div>
+                            <Typography.Title level={3}>MCQ Exam Questions</Typography.Title>
+                            <ExamDisplay 
+                                exam={exam} 
+                                currentItemId={currentItemId}
+                                setCurrentItemId={setCurrentItemId}
+                            />
+                            {exam && <ExamFileManager />}
                         </div>
-                        
-                        <Row gutter={24}>
-                            <Col xs={24} xl={sidebarCollapsed ? 24 : 18} style={{ transition: 'width 0.3s' }}>
-                                <div>
-                                    <Typography.Title level={3}>MCQ Exam Questions</Typography.Title>
-                                    <ExamDisplay 
-                                        exam={exam} 
-                                        currentItemId={currentItemId}
-                                        setCurrentItemId={setCurrentItemId}
-                                    />
-                                    {exam && <ExamFileManager />}
-                                </div>
-                            </Col>
-                            {!sidebarCollapsed && (
-                                <Col xs={6} style={{ transition: 'width 0.3s' }}>
-                                    <ExamSidebar 
-                                        exam={exam} 
-                                        currentItemId={currentItemId}
-                                        onNavigateToItem={handleNavigateToItem}
-                                        collapsed={false}
-                                        onToggleCollapse={toggleSidebar}
-                                    />
-                                </Col>
-                            )}
-                        </Row>
-                    </>
-                );
-            case 2:
-                return (
-                    <div>
-                        <Typography.Title level={3}>Export & Randomise</Typography.Title>
-                        <Typography.Paragraph type="secondary">
-                            Export functions coming soon
-                        </Typography.Paragraph>
-                        <div style={{ marginTop: 24, marginBottom: 24 }}>
-                            <Space>
-                                <Button type="primary" onClick={() => navigate('/randomiser')}>
-                                Open in Randomiser
-                                </Button>
-                            </Space>
-                            </div>
+                    </Col>
+                    {!sidebarCollapsed && (
+                        <Col xs={6} style={{ transition: 'width 0.3s' }}>
+                            <ExamSidebar 
+                                exam={exam} 
+                                currentItemId={currentItemId}
+                                onNavigateToItem={handleNavigateToItem}
+                                collapsed={false}
+                                onToggleCollapse={toggleSidebar}
+                            />
+                        </Col>
+                    )}
+                </Row>
+            </div>
 
-                            <div style={{ marginBottom: 24 }}>
-                            <Space>
-                                <Button type="default" onClick={() => {/* TODO: implement DOCX download */}}>
-                                Download as DOCX
-                                </Button>
-                                <Button type="default" onClick={() => exportExamToPdf(exam)}>
-                                Download as PDF
-                                </Button>
-                            </Space>
-                            </div>
-                    </div>
-                );
-            default:
-                return null;
-        }
-    };
+            <Divider />
 
-    return (
-        <>
-            <h1> Builder</h1>
-            <MCQBuilderProgressWrapper>
-                {(currentStep) => renderStageContent(currentStep)}
-            </MCQBuilderProgressWrapper>
-        </>
-/*
-        //Check Changes
-import { useDispatch, useSelector } from 'react-redux';
-import { createNewExam } from "../store/exam/examSlice.js";
-import { selectExamData } from "../store/exam/selectors.js";
-import { Button } from "antd";
+            {/* Export & Randomise Section */}
+            <div>
+                <Typography.Title level={3}>Export & Randomise</Typography.Title>
+                <Typography.Paragraph type="secondary">
+                    Export functions coming soon
+                </Typography.Paragraph>
+                <div style={{ marginTop: 24, marginBottom: 24 }}>
+                    <Space>
+                        <Button type="primary" onClick={() => navigate('/randomiser')}>
+                            Open in Randomiser
+                        </Button>
+                    </Space>
+                </div>
 
-const Builder = () => {
-    const exam = useSelector(selectExamData); // Get examData from redux
-    const dispatch = useDispatch();
-  
-    return (
-      <div>
-        <h1>MCQ Builder</h1>
-        {exam ? (
-          <ExamDisplay />
-        ) : (
-          <><p>No exam loaded.</p><p><Button onClick={() => dispatch(createNewExam({examTitle: 'New Exam'}))} type="primary" style={{ marginBottom: 16 }}>
-              New Exam
-            </Button></p></>
-        )}
-        <ExamFileManager />
-      </div>
-          */
+                <div style={{ marginBottom: 24 }}>
+                    <Space>
+                        <Button type="default" onClick={() => {/* TODO: implement DOCX download */}}>
+                            Download as DOCX
+                        </Button>
+                        <Button type="default" onClick={() => exportExamToPdf(exam)}>
+                            Download as PDF
+                        </Button>
+                    </Space>
+                </div>
+            </div>
+        </div>
     );
-  };
-  export default Builder;
+};
+
+export default Builder;
