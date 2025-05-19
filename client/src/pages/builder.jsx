dfc49e89f5a8204694de0623f612d9f15b5c9781
Danny Weng <danny26574@gmail.com>
2025-05-20 10:25:40 +1200

    Revised format, dropdowns changed

--- client/src/pages/builder.jsx
+++ client/src/pages/builder.jsx
@@ -5,7 +5,7 @@ import { selectExamData } from "../store/exam/selectors.js";
 import ExamDisplay from "../components/examDisplay.jsx";
 import ExamFileManager from "../components/ExamFileManager.jsx";
 import ExamSidebar from "../components/ExamSidebar.jsx";
-import { Typography, Button, Space, Row, Col, Tooltip, Collapse } from "antd";
+import { Typography, Button, Space, Row, Col, Tooltip, Collapse, Divider } from "antd";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { exportExamToPdf } from "../services/exportPdf.js";
 
@@ -28,7 +28,7 @@ const Builder = () => {
         setSidebarCollapsed(!sidebarCollapsed);
     };
 
-    const items = [
+    const coverPageItems = [
         {
             key: '1',
             label: 'Cover Page',
@@ -37,54 +37,7 @@ const Builder = () => {
                     <Typography.Title level={3}>Cover Page</Typography.Title>
                 </div>
             ),
-        },
-        {
-            key: '2',
-            label: 'MCQ Exam Questions',
-            children: (
-                <div style={{ padding: '16px 0' }}>
-                    <div>
-                        <Typography.Title level={3}>MCQ Exam Questions</Typography.Title>
-                        <ExamDisplay 
-                            exam={exam} 
-                            currentItemId={currentItemId}
-                            setCurrentItemId={setCurrentItemId}
-                        />
-                        {exam && <ExamFileManager />}
-                    </div>
-                </div>
-            ),
-        },
-        {
-            key: '3',
-            label: 'Export & Randomise',
-            children: (
-                <div style={{ padding: '16px 0' }}>
-                    <Typography.Title level={3}>Export & Randomise</Typography.Title>
-                    <Typography.Paragraph type="secondary">
-                        Export functions coming soon
-                    </Typography.Paragraph>
-                    <div style={{ marginTop: 24, marginBottom: 24 }}>
-                        <Space>
-                            <Button type="primary" onClick={() => navigate('/randomiser')}>
-                                Open in Randomiser
-                            </Button>
-                        </Space>
-                    </div>
-
-                    <div style={{ marginBottom: 24 }}>
-                        <Space>
-                            <Button type="default" onClick={() => {/* TODO: implement DOCX download */}}>
-                                Download as DOCX
-                            </Button>
-                            <Button type="default" onClick={() => exportExamToPdf(exam)}>
-                                Download as PDF
-                            </Button>
-                        </Space>
-                    </div>
-                </div>
-            ),
-        },
+        }
     ];
 
     return (
@@ -108,11 +61,54 @@ const Builder = () => {
             <Row gutter={24}>
                 <Col xs={24} xl={sidebarCollapsed ? 24 : 18} style={{ transition: 'width 0.3s' }}>
                     <h1>Builder</h1>
+                    
+                    {/* Cover Page Section */}
                     <Collapse 
-                        defaultActiveKey={['1', '2', '3']} 
-                        items={items}
+                        defaultActiveKey={[]} 
+                        items={coverPageItems}
                         style={{ marginTop: '16px' }}
                     />
+
+                    <Divider />
+
+                    {/* MCQ Exam Questions Section */}
+                    <div style={{ marginTop: '24px' }}>
+                        <Typography.Title level={3}>MCQ Exam Questions</Typography.Title>
+                        <ExamDisplay 
+                            exam={exam} 
+                            currentItemId={currentItemId}
+                            setCurrentItemId={setCurrentItemId}
+                        />
+                        {exam && <ExamFileManager />}
+                    </div>
+
+                    <Divider />
+
+                    {/* Export & Randomise Section */}
+                    <div style={{ marginTop: '24px' }}>
+                        <Typography.Title level={3}>Export & Randomise</Typography.Title>
+                        <Typography.Paragraph type="secondary">
+                            Export functions coming soon
+                        </Typography.Paragraph>
+                        <div style={{ marginTop: 24, marginBottom: 24 }}>
+                            <Space>
+                                <Button type="primary" onClick={() => navigate('/randomiser')}>
+                                    Open in Randomiser
+                                </Button>
+                            </Space>
+                        </div>
+
+                        <div style={{ marginBottom: 24 }}>
+                            <Space>
+                                <Button type="default" onClick={() => {/* TODO: implement DOCX download */}}>
+                                    Download as DOCX
+                                </Button>
+                                <Button type="default" onClick={() => exportExamToPdf(exam)}>
+                                    Download as PDF
+                                </Button>
+                            </Space>
+                        </div>
+                    </div>
                 </Col>
                 {!sidebarCollapsed && (
                     <Col xs={6} style={{ transition: 'width 0.3s' }}>
